489d3174c507f339d186be532a21fba2e6438b19
VasilKakevski <kakevski@abv.bg>
2017-07-08 13:39:59 +0300

    Add files via upload

---
+++ Pepi_Roulette_SDL/src/Globals.h
@@ -0,0 +1,19 @@
+/*
+ * Globals.h
+ *
+ *  Created on: Jul 8, 2017
+ *      Author: kakev
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+#include <SDL2/SDL.h>
+
+
+static SDL_Renderer * gRenderer = NULL;
+static SDL_Window * gWindow = NULL;
+const int SCREEN_W = 800;
+const int SCREEN_H = 600;
+
+
+#endif /* GLOBALS_H_ */
