b0294232b82ab6d236a6daf4e5d37c19a1f6f0cf
VasilKakevski <kakevski@abv.bg>
2017-07-08 13:36:29 +0300

    Add files via upload

---
+++ src/Game.h
@@ -0,0 +1,17 @@
+/*
+ * Game.h
+ *
+ *  Created on: Jul 7, 2017
+ *      Author: kakev
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game {
+public:
+	Game();
+	virtual ~Game();
+};
+
+#endif /* GAME_H_ */
--- src/Globals.h
+++ src/Globals.h
@@ -1,15 +1,19 @@
+/*
+ * Globals.h
+ *
+ *  Created on: Jul 8, 2017
+ *      Author: kakev
+ */
+
 #ifndef GLOBALS_H_
 #define GLOBALS_H_
+#include <SDL2/SDL.h>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 400;
-
-const double DENOMINATION = 0.01;
-const int MULTIPLIER_NUMBER = 36;
-const int MULTIPLIER_OTHER = 2;
-
-const int BUTTON_NUMBERS = 37;
 
 static SDL_Renderer * gRenderer = NULL;
+static SDL_Window * gWindow = NULL;
+const int SCREEN_W = 800;
+const int SCREEN_H = 600;
 
-#endif 
+
+#endif /* GLOBALS_H_ */
